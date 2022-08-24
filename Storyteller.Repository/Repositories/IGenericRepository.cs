fc0afd2e93e4d2b761bcf99579aae97d48b26eba
OSSIxD <agpodymniak@gmail.com>
2022-08-24 10:39:35 +0200

    Added simple auth crud with generic repository pattern

---
+++ Storyteller.Repository/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storyteller.Repository.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetList();
+        T Get(int ID);
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(int ID);
+    }
+}
